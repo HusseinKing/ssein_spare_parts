23c3d8ae3812827574a6afdba5229aff13d8f302
“ALVINdimpos” <sadoug013@gmail.com>
2024-11-05 23:18:15 +0200

    fix: can not read property id

--- src/dashboard/data/index.js
+++
@@ -1,8 +0,0 @@
-export { productTableData as productTableData } from "./product-table-data";
-export { statisticsCardsData as statisticsCardsData } from "./statistics-cards-data";
-export { userTableData as userTableData } from "./user-table-data";
-export { default as reportData } from "./report-data";
-export { debtorsData as debtorsData } from "./debtors-data";
-export { creditorsData as creditorsData } from "./creditors-data";
-export { queriesData as queriesData } from "./queries-data";
-export { cashBookDatas as cashBookDatas } from "./cash-book-data";
